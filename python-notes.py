611372c19896ee5d23eaceb00b119f400e476375
anieranki <73202953+anieranki@users.noreply.github.com>
2020-11-09 19:54:34 -0800

    Update python-notes.py

--- python-notes.py
+++ python-notes.py
@@ -1,4 +1,12 @@
+#squares all numbers provided in input data set and gives output, using for loop
 def square(lst):
     for idx in range(len(lst)):
         lst[idx] = lst[idx]**2
     return lst
+
+#importing math module to display the value of pi
+#others include sin, sinh, sqrt, tan, tau, trunc, values
+import math
+print("The approximation for the value of pi is",math.pi)
+
+#
